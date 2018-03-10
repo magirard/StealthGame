111bc0bfa3ffd242237ba01176aa812cc13aa054
magirard <marcandre.girard@gmail.com>
2018-03-10 17:40:23 -0500

    Section 3 Lecture 17 - Adding a basic extraction zone

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSExtractionZone.h"
+
+#include "Components/BoxComponent.h"
+#include "Components/PrimitiveComponent.h"
+
+AFPSExtractionZone::AFPSExtractionZone()
+{
+	TriggerZone = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerZone"));
+	TriggerZone->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	TriggerZone->SetCollisionResponseToAllChannels(ECR_Ignore);
+	TriggerZone->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	TriggerZone->SetBoxExtent(FVector(200));
+	TriggerZone->SetHiddenInGame(false);
+
+	RootComponent = TriggerZone;
+
+	TriggerZone->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::OnHeroOverlap);
+}
+
+
+void AFPSExtractionZone::OnHeroOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone"));
+}
+
---
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FPSExtractionZone.generated.h"
+
+
+class UBoxComponent;
+class UPrimitiveComponent;
+
+UCLASS()
+class FPSGAME_API AFPSExtractionZone : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AFPSExtractionZone();
+
+protected:
+
+
+	UPROPERTY(EditDefaultsOnly, Category = "Triggers")
+		UBoxComponent* TriggerZone;
+
+	UFUNCTION()
+	void OnHeroOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+	
+};
