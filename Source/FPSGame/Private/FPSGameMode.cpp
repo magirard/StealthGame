0c8e5086b9d9d9a623f0323de6cd7f9d516a80a5
magirard <marcandre.girard@gmail.com>
2018-03-11 09:33:04 -0400

    Section 3 Lecture 21 - End of game transition in C++

--- Source/FPSGame/Private/FPSGameMode.cpp
+++ Source/FPSGame/Private/FPSGameMode.cpp
@@ -4,6 +4,7 @@
 #include "FPSHUD.h"
 #include "FPSCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
 
 AFPSGameMode::AFPSGameMode()
 {
@@ -20,6 +21,32 @@ void AFPSGameMode::CompleteMission(APawn* instigatorPawn)
 	if (instigatorPawn)
 	{
 		instigatorPawn->DisableInput(nullptr);
+
+		if (SpectatingViewPointClass)
+		{
+			APlayerController* PC = Cast<APlayerController>(instigatorPawn->GetController());
+			if (PC)
+			{
+				AActor* NewViewTarget = nullptr;
+				TArray<AActor*> ReturnedActors;
+				UGameplayStatics::GetAllActorsOfClass(this, SpectatingViewPointClass, ReturnedActors);
+				if (ReturnedActors.Num() > 0)
+				{
+					NewViewTarget = ReturnedActors[0];
+					FViewTargetTransitionParams params;
+					params.BlendTime = 1.0f;
+					params.BlendFunction = EViewTargetBlendFunction::VTBlend_Cubic;
+					PC->SetViewTarget(NewViewTarget, params);
+				}
+
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("SpectatingViewPointClass is nullptr, Please update gamemode class with valid subclass"));
+		}
 	}
 	OnMissionCompleted(instigatorPawn);
+
+
 }
